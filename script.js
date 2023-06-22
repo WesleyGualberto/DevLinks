582f600a46a9dc0dfe492d5fa9f3163d25b107f6
Wesley Gualberto <wesleygualberto0@gmail.com>
2023-06-22 16:37:48 -0300

    :sparkles: Add support different resolutions!

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ function toggleMode() {
 
   // Subistituir a imagem
   if (html.classList.contains("dark")) {
-    // se estiver em (light mode), adicionar a imagem light
+    // se estiver em (dark mode), manter a imagem principal.
+    img.setAttribute("src", "./assets/avatar.png")
+  } else {
+    // se não estiver em (dark mode), adicionar a imagem light.
     img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brito sorrindo, usando óculus escuro e jaqueta preta com fundo degradê roxo com azul"
     )
-  } else {
-    // se não estiver em (light mode), manter a imagem principal
-    img.setAttribute("src", "./assets/avatar.png")
   }
 }
