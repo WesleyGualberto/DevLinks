57974527d763927625bf52deb9a78a01c17d015e
Wesley Gualberto <wesleygualberto0@gmail.com>
2023-06-22 09:07:10 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode() {
+  // Pegar a tag HTML element
+  const html = document.documentElement
+
+  // Propriedade que identifica um atributo para uma codição "true" or "false".
+  html.classList.toggle("dark")
+
+  // Pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // Subistituir a imagem
+  if (html.classList.contains("dark")) {
+    // se estiver em (light mode), adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculus escuro e jaqueta preta com fundo degradê roxo com azul"
+    )
+  } else {
+    // se não estiver em (light mode), manter a imagem principal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
