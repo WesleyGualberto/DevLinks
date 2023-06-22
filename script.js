842606ab8ca22e6c8afc186441835870fcfc3533
Wesley Gualberto <wesleygualberto0@gmail.com>
2023-06-22 17:07:47 -0300

    :ok_hand: Adjusted class of toggleMode

--- script.js
+++ script.js
@@ -3,21 +3,22 @@ function toggleMode() {
   const html = document.documentElement
 
   // Propriedade que identifica um atributo para uma codição "true" or "false".
-  html.classList.toggle("dark")
+  html.classList.toggle("light")
 
   // Pegar a tag img
   const img = document.querySelector("#profile img")
 
   // Subistituir a imagem
-  if (html.classList.contains("dark")) {
-    // se estiver em (dark mode), manter a imagem principal.
-    img.setAttribute("src", "./assets/avatar.png")
+  if (html.classList.contains("light")) {
   } else {
-    // se não estiver em (dark mode), adicionar a imagem light.
+    // se estiver em (light mode), altera a imagem para light.
     img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute(
       "alt",
       "Foto de Mayk Brito sorrindo, usando óculus escuro e jaqueta preta com fundo degradê roxo com azul"
     )
+
+    // se não estiver (light mode), manter a imagem principal.
+    img.setAttribute("src", "./assets/avatar.png")
   }
 }
